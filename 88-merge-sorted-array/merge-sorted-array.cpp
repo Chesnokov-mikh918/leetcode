0afa3702ae65591b64e472b8a2540fa21f3de032
Chesnokov-mikh918 <mikhail.chesnokov.1212@gmail.com>
2026-03-31 12:09:52 +0300

    Time: 0 ms (100.00%) | Memory: 12.3 MB (72.00%) - LeetSync

---
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int p1 = m - 1;
+        int p2 = n - 1;
+        int p3 = m + n - 1;
+
+        if (m == 0) {
+            for (int i = 0; i < n; i++) {
+                nums1[i] = nums2[i];
+            }
+        }
+
+        while (p1 >= 0 && p2 >= 0) {
+            if (nums1[p1] <= nums2[p2]) {
+                nums1[p3] = nums2[p2];
+                p2--;
+                p3--;
+            }
+            else if (nums1[p1] > nums2[p2]) {
+                nums1[p3] = nums1[p1];
+                p3--;
+                p1--;
+            }
+            if (p2 < 0) {
+                break;
+            }
+            if (p1 < 0) {
+                while (p2 >= 0) {
+                    nums1[p3] = nums2[p2];
+                    p2--;
+                    p3--;
+                }
+            }
+        }
+    }
+};
