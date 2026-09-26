a92280b5862966d89b4491be2a14bedceab3aa6b
Chesnokov-mikh918 <mikhail.chesnokov.1212@gmail.com>
2026-09-26 16:36:45 +0300

    Time: 68 ms (17.76%) | Memory: 51.5 MB (61.68%) - LeetSync

--- 1934-evaluate-the-bracket-pairs-of-a-string/evaluate-the-bracket-pairs-of-a-string.py
+++ 1934-evaluate-the-bracket-pairs-of-a-string/evaluate-the-bracket-pairs-of-a-string.py
@@ -1,25 +1,25 @@
 class Solution:
     def evaluate(self, s: str, knowledge: list[list[str]]) -> str:
-        ans_str = ""
         first = 0
         second = 0
         knowledge_map = {}
         for element in knowledge:
             knowledge_map[element[0]] = element[1]
 
+        parts = []
         while first < len(s):
             if s[first] != "(":
-                ans_str += s[first]
+                parts.append(s[first])
                 first += 1
             else:
                 while s[second] != ")":
                     second += 1
                 key_word = s[first + 1:second]
                 if key_word not in knowledge_map.keys():
-                    ans_str += '?'
+                    parts.append('?')
                 else:
-                    ans_str += knowledge_map[key_word]
+                    parts.append(knowledge_map[key_word])
                 first = second + 1
                 second += 1
 
-        return ans_str
+        return "".join(parts)
